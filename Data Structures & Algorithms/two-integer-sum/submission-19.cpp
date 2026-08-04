224d453c6a9a480a6915144f385576ad8f4fd6de
lordsuirules-blip <lordsuirules@gmail.com>
2026-08-04 20:38:46 +0530

    Add: two-integer-sum - submission-19

---
+++ Data Structures & Algorithms/two-integer-sum/submission-19.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> um;
+
+        for(int i = 0; i < nums.size(); i++){
+            auto it = um.find(target - nums[i]);
+            if(it != um.end())
+                return {it->second,i};
+            
+            um[nums[i]] = i;
+        }
+        return{};
+    }
+};
