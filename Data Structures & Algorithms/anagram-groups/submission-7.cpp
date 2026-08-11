f059901d37d024014d75646436bd10021d589027
rutvik-chandra-m1 <lordsuirules@gmail.com>
2026-08-12 05:14:41 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+   public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> res;
+        for (auto& [k,i] : mp) res.push_back(i);
+        return res;
+    }
+};
