f3b600dbe3f042fb977ebee7eade0ba96ae19a7c
rutvik-chandra-m1 <lordsuirules@gmail.com>
2026-08-20 18:36:30 +0530

    Add: two-integer-sum - submission-24

---
+++ Data Structures & Algorithms/two-integer-sum/submission-24.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> freq;
+
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = freq.find(target - nums[i]);
+            if (it != freq.end()) {
+                return {it->second, i};
+            }
+            freq[nums[i]] = i;
+        }
+        return{};
+    }
+};
